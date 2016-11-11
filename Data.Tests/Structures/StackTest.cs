414b04544782b9c44f43bcbf13347c3f4ba46ded
Alan Camargo <alan.camargo@intra.labone.net>
2016-11-11 11:04:01 -0200

    Stack ready and tested

---
+++ Data.Tests/Structures/StackTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data.Structures;
+
+namespace Data.Tests.Structures
+{
+
+    [TestClass]
+    public class StackTest
+    {
+
+        [TestMethod]
+        public void ShouldPushElement()
+        {
+            Stack<int> stack = new Stack<int>();
+            Assert.IsTrue(stack.IsEmpty);
+            int element = 1;
+            stack.Push(element);
+            Assert.AreEqual(1, stack.Count);
+        }
+
+        [TestMethod]
+        public void ShouldPopElement()
+        {
+            Stack<int> stack = new Stack<int>();
+            int element = 1;
+            stack.Push(element);
+            int removed = stack.Pop();
+            Assert.IsTrue(stack.IsEmpty);
+            Assert.AreEqual(element, removed);
+        }
+
+        [TestMethod]
+        public void ShouldGetTopElement()
+        {
+            Stack<int> stack = new Stack<int>();
+            int element = 1;
+            stack.Push(element);
+            Assert.AreEqual(element, stack.Top);
+        }
+
+    }
+
+}
