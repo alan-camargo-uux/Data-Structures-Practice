4e93d76f606147d5e0559929c84bb13230acccef
alan-camargo-uux <alan.camargo@uux.com>
2016-11-12 13:03:18 -0200

    Queue ready and tested

--- Data/Structures/Queue.cs
+++ Data/Structures/Queue.cs
@@ -60,9 +60,8 @@ namespace Data.Structures
                 front = node;
             else
             {
-                // FIXME: fix Queue.Enqueue
                 Node<T> back = front;
-                for (int i = 0; i < Count; i++)
+                while (back.Next != default(Node<T>))
                     back = back.Next;
                 back.Next = node;
             }
